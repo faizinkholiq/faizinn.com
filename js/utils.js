0c72a5d139e835c3cdd9ac683a55dc92b2ef2ef9
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-02-20 03:04:39 +0700

    initial

---
+++ js/utils.js
@@ -0,0 +1,20 @@
+window.onclick = function(event) {
+  if (!event.target.matches(".dropdown-trigger")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+const dropdownMenu = (id) => {
+  document.getElementById(id).classList.toggle("show");
+}
+
+const goTo = (url) => {
+  window.open(url, '_self');
+} 
